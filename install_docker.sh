df378ab0289e84a479464c71951183158486a363
MakalisterAndrade <95728126+MakalisterAndrade@users.noreply.github.com>
2024-09-03 14:57:45 -0400

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -4,3 +4,6 @@ curl -sSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 rm get-docker.sh
 sudo apt install docker-compose -y
+sudo usermod -aG docker $USER
+sudo chown root:docker /var/run/docker.sock
+sudo chmod 660 /var/run/docker.sock
